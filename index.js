64374954e1d717eac7a1383fd4280230acfa6493
YangSiJun528 <80192911+YangSiJun528@users.noreply.github.com>
2021-09-30 20:30:19 +0900

    post 요청 처리

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const app = express();
 const PORT = process.env.port || 8000;
 const path = require('path');
 
-const http = require('http').createServer(app);
-
 app.use(express.urlencoded({extended: true})) 
 
+const http = require('http').createServer(app);
+
 app.use(express.static(path.join(__dirname, '../CLIENT/build')));
+
+app.post('/login', function (req, res) {
+    console.log(req)
+});
 
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, '../CLIENT/build/index.html'));
@@ -15,4 +19,4 @@ app.get('*', function (req, res) {
 
 app.listen(PORT, ()=>{
     console.log(`running on port ${PORT}`);
-});
+}); 
