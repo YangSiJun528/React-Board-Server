b47c27f241220517233cafe949e0f39af638c5f1
YangSiJun528 <80192911+YangSiJun528@users.noreply.github.com>
2021-09-30 17:58:20 +0900

    React 연동

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.port || 8000;
+const path = require('path');
+
+const http = require('http').createServer(app);
+
+app.use(express.urlencoded({extended: true})) 
+
+app.use(express.static(path.join(__dirname, '../CLIENT/build')));
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../CLIENT/build/index.html'));
+});
 
 app.listen(PORT, ()=>{
     console.log(`running on port ${PORT}`);
